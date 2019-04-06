bc383eb9e508e741b89109d86b41e455fb7b0901
Admin <admin@mac-admin.local>
2019-04-06 16:01:55 +0300

    Initial Commit

---
+++ MessageAR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MessageAR
+//
+//  Created by Admin on 06.04.2019.
+//  Copyright © 2019 goncharov denis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
